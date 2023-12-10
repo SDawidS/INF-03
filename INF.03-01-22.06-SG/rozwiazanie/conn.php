d7a07c5cbe348b234b060247987587751eb822b6
SDawidS <63451295+SDawidS@users.noreply.github.com>
2023-12-10 10:25:47 +0000

    Update conn.php

--- INF.03-01-22.06-SG/rozwiazanie/conn.php
+++ INF.03-01-22.06-SG/rozwiazanie/conn.php
@@ -7,8 +7,6 @@ $dbname = "wedkuj";
 
 $conn = mysqli_connect($server, $user, $pass, $dbname);
 if(!$conn){
-    mysqli_error($conn);
-    mysqli_close($conn);
+    die("Polaczenie sie nie powiodlo: ".mysqli_connect_error());
 }
-
 ?>
